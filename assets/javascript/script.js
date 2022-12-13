617e3a4fb26b8bea2cfc51d1d648e72161839ee4
Deanbert <deanfay2000@gmail.com>
2022-12-13 23:36:16 +0000

    Amend JS addTodo function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 //Selectors
-const inputBox = document.querySelector('.input-box')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
+const inputBox = document.querySelector('.input-box');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener("click", addToDo);
@@ -10,26 +10,26 @@ todoButton.addEventListener("click", addToDo);
 function addTodo(event) {
 
     //Used to create a li and add it to the todo-list container
-    const todoContainer = document.createElement('li');
-
-    //Used to a class for styling newly added items
-    todoContainer.classList.add('todo-item');
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
 
     //Adds new todo item to todo container
-    todoContainer.appendChild(newTodo);
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
 
     //Adds tickbox for tasks that are complete
     const completeButton = document.createElement('button');
     completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     completeButton.classList.add('complete-button');
-    todoContainer.appendChild(completeButton);
+    todoDiv.appendChild(completeButton);
 
     //Adds delete button for removing added tasks
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
     deleteButton.classList.add('complete-button');
-    todoContainer.appendChild(deleteButton);
+    todoDiv.appendChild(deleteButton);
 
     //Append item to list
-    todoContainer.appendChild(todoContainer);
+    todoList.appendChild(todoDiv);
 }
