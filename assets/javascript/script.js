f2339d7d83e8c778ed13e6a8e3065bea5a3b07ad
Deanbert <deanfay2000@gmail.com>
2022-12-13 21:20:34 +0000

    Add JS selectors and event listeners

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+//Selectors
+const inputBox = document.querySelector('.input-box')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+//Event Listeners
+todoButton.addEventListener("click", addToDo);
